5ed8d0f87cce8cd5065e0f10703ae40d950cc7b4
Will Jardine <will@wearewith.com>
2016-04-16 11:09:52 -0400

    Stubbed in CSS and JavaScript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,43 @@
-console.log('JavaScript loaded.');
+
+//////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////
+
+	var GRID_WIDTH = 16,
+		GRID_HEIGHT = 16,
+		GAME_WIDTH = GRID_WIDTH * 9,
+		GAME_HEIGHT = GRID_HEIGHT * 6;
+
+
+//////////////////////////////////////////////////
+// SETUP CANVAS
+//////////////////////////////////////////////////
+
+	var canvas = document.getElementById('canvas');
+	canvas.width = GAME_WIDTH;
+	canvas.height = GAME_HEIGHT;
+
+
+//////////////////////////////////////////////////
+// RESIZE WINDOW
+//////////////////////////////////////////////////
+
+	function resizeWindow() {
+		var winWidth = Math.floor(window.innerWidth / GRID_WIDTH) * GRID_WIDTH;
+		var winHeight = Math.floor(window.innerHeight / GRID_HEIGHT) * GRID_HEIGHT;
+		var winRatio = winWidth / winHeight;
+		var imgRatio = GAME_WIDTH / GAME_HEIGHT;
+		var newWidth, newHeight;
+		if (winRatio < imgRatio) {
+			newWidth = winWidth;
+			newHeight = (winWidth / GAME_WIDTH) * GAME_HEIGHT;
+		} else {
+			newWidth = (winHeight / GAME_HEIGHT) * GAME_WIDTH;
+			newHeight = winHeight;
+		}
+		canvas.style.width = newWidth + 'px';
+		canvas.style.height = newHeight + 'px';
+	}
+	window.addEventListener('resize', resizeWindow);
+	window.addEventListener('orientationchange', resizeWindow);
+	resizeWindow();
