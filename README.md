e6265d4d2b7faf281f6726964bb0032e60fb1f17
Will Jardine <will@wearewith.com>
2016-04-17 01:01:46 -0400

    Added player movement and collisions to solid tiles within tilemap.

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ This is my first game jam! I'm planning on using:
 * [cfxr](http://thirdcog.eu/apps/cfxr)
 * [PixiTracker 1Bit](http://www.warmplace.ru/soft/pixitracker/)
 * [Photoshop](http://www.adobe.com/products/photoshop.html)
+
+## Prototype's Assets
+
+* [Classic Hero and Baddies Pack](http://opengameart.org/content/classic-hero-and-baddies-pack)
+* [Items and Elements](http://opengameart.org/content/items-and-elements)
+* [Platformer Baddies](http://opengameart.org/content/platformer-baddies)
+* [Seasonal Platformer Tiles](http://opengameart.org/content/seasonal-platformer-tiles)
--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,16 @@
 		}
 	];
 
+	var PLAYER_HITAREA_WIDTH = 12;
+	var PLAYER_HITAREA_HEIGHT = 12;
+
+	var PLAYER_MOVE_SPEED = 64;
+	var PLAYER_JUMP_SPEED = 180;
+	var PLAYER_GRAVITY_ACCELERATION = 400;
+	var PLAYER_MAX_FALL_SPEED = 180;
+	var PLAYER_
+
+
 
 //////////////////////////////////////////////////
 // VARIABLES
@@ -42,9 +52,22 @@
 	var playerSpriteSheet, tilesSpriteSheet;
 	var background, fpsLabel, player, room;
 
+	var currentLevel = 0;
+
 	var jumpKeyHeld = false;
 	var leftKeyHeld = false;
 	var rightKeyHeld = false;
+
+
+//////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////
+
+	Math.clamp = function(value, min, max) {
+		value = value > max ? max : value;
+		value = value < min ? min : value;
+		return value;
+	};
 
 
 //////////////////////////////////////////////////
@@ -64,10 +87,11 @@
 		}
 
 		// canvas
-		canvas = document.getElementById('canvas');
+		canvas = document.createElement('canvas');
 		canvas.imageSmoothingEnabled = false;
 		canvas.width = GAME_WIDTH;
 		canvas.height = GAME_HEIGHT;
+		document.body.appendChild(canvas);
 
 		// window
 		window.addEventListener('resize', resizeWindow);
@@ -82,7 +106,6 @@
 		manifest = [
 			// images
 			{id:'background',	src:'assets/img/background.gif'},
-			{id:'boom',			src:'assets/img/boom.gif'},
 			{id:'coin',			src:'assets/img/coin.gif'},
 			{id:'player',		src:'assets/img/player.gif'},
 			{id:'spikes',		src:'assets/img/spikes.gif'},
@@ -143,8 +166,13 @@
 			animations: tilesSpriteSheetAnimations
 		});
 
-		// setup room
+		// player
 		player = new createjs.Sprite(playerSpriteSheet, 'idle');
+		player.currentAnimation = 'idle';
+		player.velocity = {x:0, y:0};
+		player.onGround = false;
+
+		// room
 		room = new createjs.Container();
 		stage.addChild(room);
 		changeLevel(0);
@@ -154,7 +182,7 @@
 
 		// start game loop
 		createjs.Ticker.timingMode = createjs.Ticker.RAF;
-		createjs.Ticker.addEventListener('tick', tick);
+		createjs.Ticker.addEventListener('tick', update);
 
 		// register events
 		//canvas.onclick = handleClick;
@@ -172,25 +200,185 @@
 
 		room.removeAllChildren();
 		jumpKeyHeld = leftKeyHeld = rightKeyHeld = false;
+		currentLevel = level;
 
 		var map = LEVELS[level].map;
 		for (var y=0; y<map.length; ++y) {
 			for (var x=0; x<map[y].length; ++x) {
 				if (parseInt(map[y][x]) == 1) {
 					var tile = new createjs.Sprite(tilesSpriteSheet, 'tile-16');
-					tile.x = x * GRID_WIDTH;
-					tile.y = y * GRID_WIDTH;
+					tile.x = getTileX(x);
+					tile.y = getTileY(y);
 					room.addChild(tile);
 				}
 			}
 		}
 
+		player.currentAnimation = 'idle';
 		player.x = (LEVELS[level].player[0] * GRID_WIDTH) + (GRID_WIDTH / 2);
 		player.y = (LEVELS[level].player[1] * GRID_HEIGHT) + (GRID_HEIGHT / 2);
+		player.velocity.x = 0;
+		player.velocity.y = 0;
+		player.scaleX = 1;
 		player.gotoAndPlay('idle');
 		room.addChild(player);
 
 		stage.update();
+	}
+
+
+//////////////////////////////////////////////////
+// UPDATE LOOP
+//////////////////////////////////////////////////
+
+	function update(event) {
+
+		var deltaTime = event.delta / 1000;
+
+		// 1. update enemies
+
+		updatePlayer(deltaTime);
+
+		// 3. check for enemy/player collisions
+
+		// 4. check for item/player collisions
+
+		// 5. update camera
+
+		fpsLabel.text = Math.round(createjs.Ticker.getMeasuredFPS()) + ' fps';
+		stage.update(event);
+	}
+	function updatePlayer(deltaTime) {
+
+		// remember current animation
+		var animation = player.currentAnimation;
+
+		// get direction
+		var dirX = 0;
+		dirX += leftKeyHeld ? -1 : 0;
+		dirX += rightKeyHeld ? 1 : 0;
+
+		// update velocity
+		player.velocity.x = dirX * PLAYER_MOVE_SPEED;
+		player.velocity.y = Math.clamp(player.velocity.y + PLAYER_GRAVITY_ACCELERATION * deltaTime, -PLAYER_MAX_FALL_SPEED, PLAYER_MAX_FALL_SPEED);
+
+		// start jumping?
+		if (jumpKeyHeld && player.onGround) {
+			animation = 'jump';
+			player.velocity.y = -PLAYER_JUMP_SPEED;
+		} if (dirX !== 0) {
+			animation = 'walk';
+		} else {
+			animation = 'idle';
+		}
+
+		// update x position
+		player.x += player.velocity.x * deltaTime;
+		if (player.x < PLAYER_HITAREA_WIDTH / 2) {
+			player.x = PLAYER_HITAREA_WIDTH / 2;
+			player.velocity.x = 0;
+			animation = (animation === 'walk') ? 'idle' : animation;
+		} else if (player.x > (LEVELS[currentLevel].map[0].length * GRID_WIDTH) - (PLAYER_HITAREA_WIDTH / 2)) {
+			player.x = (LEVELS[currentLevel].map[0].length * GRID_WIDTH) - (PLAYER_HITAREA_WIDTH / 2);
+			player.velocity.x = 0;
+			animation = (animation === 'walk') ? 'idle' : animation;
+		}
+		updateTileCollision(player, dirX, 0, PLAYER_HITAREA_WIDTH, PLAYER_HITAREA_HEIGHT);
+
+		// update y position
+		var dirY = 0;
+		dirY = (player.velocity.y < 0) ? -1 : dirY;
+		dirY = (player.velocity.y > 0) ? 1 : dirY;
+		player.y += player.velocity.y * deltaTime;
+		if (player.y < PLAYER_HITAREA_HEIGHT / 2) {
+			player.y = PLAYER_HITAREA_HEIGHT / 2;
+			player.velocity.y = 0;
+			//animation = (animation === 'walk') ? 'idle' : animation;
+		} else if (player.y > (LEVELS[currentLevel].map.length * GRID_HEIGHT) - (PLAYER_HITAREA_HEIGHT / 2)) {
+			player.y = (LEVELS[currentLevel].map.length * GRID_HEIGHT) - (PLAYER_HITAREA_HEIGHT / 2);
+			player.velocity.y = 0;
+			animation = 'fall';
+		}
+		//player.y = Math.clamp(player.y, PLAYER_HITAREA_HEIGHT / 2, (LEVELS[currentLevel].map.length * GRID_HEIGHT) - (PLAYER_HITAREA_HEIGHT / 2));
+		updateTileCollision(player, 0, dirY, PLAYER_HITAREA_WIDTH, PLAYER_HITAREA_HEIGHT);
+
+		// on ground?
+		player.onGround = isSolidTileAt(player.x, player.y + (PLAYER_HITAREA_WIDTH / 2));
+
+		// update flipping
+		player.scaleX = (dirX < 0) ? -1 : player.scaleX;
+		player.scaleX = (dirX > 0) ? 1 : player.scaleX;
+
+		//animation = 'fall';
+
+		// update animation
+		if (animation !== player.currentAnimation) {
+			player.currentAnimation = animation;
+			player.gotoAndPlay(animation);
+		}
+
+	}
+
+
+//////////////////////////////////////////////////
+// TILEMAP
+//////////////////////////////////////////////////
+
+	function updateTileCollision(element, dirX, dirY, width, height) {
+
+		var row, col;
+
+		var left = element.x - (width / 2);
+		var right = element.x + (width / 2) - 0.5;
+		var top = element.y - (height / 2);
+		var bottom = element.y + (height / 2) - 0.5;
+
+		// check for collisions on sprite sides
+		var collision =
+			isSolidTileAt(left, top) ||
+			isSolidTileAt(right, top) ||
+			isSolidTileAt(right, bottom) ||
+			isSolidTileAt(left, bottom);
+		if (collision === false) {
+			return;
+		}
+
+		if (dirY > 0) {
+			row = getTileRow(bottom);
+			element.y = -(height / 2) + getTileY(row);
+			element.velocity.y = 0;
+		} else if (dirY < 0) {
+			row = getTileRow(top);
+			element.y = (height / 2) + getTileY(row + 1);
+			element.velocity.y = 0;
+		} else if (dirX > 0) {
+			col = getTileCol(right);
+			element.x = -(width / 2) + getTileX(col);
+			element.velocity.x = 0;
+		} else if (dirX < 0) {
+			col = getTileCol(left);
+			element.x = (width / 2) + getTileX(col + 1);
+			element.velocity.x = 0;
+		}
+
+	}
+	function isSolidTileAt(x, y) {
+		var col = Math.floor(x / GRID_WIDTH);
+		var row = Math.floor(y / GRID_HEIGHT);
+		var tile = LEVELS[currentLevel].map[row][col];
+		return (tile > 0) ? true : false;
+	}
+	function getTileCol(x) {
+		return Math.floor(x / GRID_WIDTH);
+	}
+	function getTileRow(y) {
+		return Math.floor(y / GRID_HEIGHT);
+	}
+	function getTileX(col) {
+		return col * GRID_WIDTH;
+	}
+	function getTileY(row) {
+		return row * GRID_HEIGHT;
 	}
 
 
@@ -247,32 +435,6 @@
 				rightKeyHeld = false;
 				break;
 		}
-	}
-
-
-//////////////////////////////////////////////////
-// UPDATE LOOP
-//////////////////////////////////////////////////
-
-	function tick(event) {
-
-		/*
-		var deltaTime = event.delete / 1000;
-
-		if (jumpKeyHeld) {
-			player.gotoAndPlay('jump');
-		}
-		if (leftKeyHeld) {
-			player.scaleX = -1;
-		}
-		if (rightKeyHeld) {
-			player.gotoAndPlay('walk');
-			player.scaleX = 1;
-		}
-		*/
-
-		fpsLabel.text = Math.round(createjs.Ticker.getMeasuredFPS()) + ' fps';
-		stage.update(event);
 	}
 
 
